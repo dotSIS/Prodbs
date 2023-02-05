33c91a509d0f2923575197bd19a87be677d69ec5
Aquilino Cagubcub <56160486+linoqui14@users.noreply.github.com>
2023-02-06 04:32:58 +0800

    Added Rest API for Products and user

--- backbone/prodbs/api/urls.py
+++ backbone/prodbs/api/urls.py
@@ -1,6 +1,10 @@
 
 from django.urls import path
+from .views import *
 # from .views import 
 urlpatterns = [
-   
+    path('users/',UserList.as_view()),
+    path('users/<int:pk>/',UserDetails.as_view()),
+    path('products/',ProductList.as_view()),
+    path('products/<int:pk>/',ProductDetails.as_view()),
 ]
--- backbone/prodbs/api/views.py
+++ backbone/prodbs/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class UserList(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+        
+
+class UserDetails(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class ProductList(generics.ListCreateAPIView):
+    serializer_class = ProductSerializer
+    def get_queryset(self):
+        queryset = Products.objects.all()
+        user = self.request.query_params.get('users')
+        if user is not None:
+            queryset = queryset.filter(sellerID=user)
+        return queryset
+
+class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Products.objects.all()
