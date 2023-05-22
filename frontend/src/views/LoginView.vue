d1227425ad6bee7217ecb14708cf2b6d46a9b340
dotSIS <sjezzrey@gmail.com>
2023-05-22 20:47:57 +0800

    Updated LoginView.vue

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,7 +3,6 @@
         <h1>Login Page</h1>
         <div class="options">
             <router-link :to="{ name: 'AmazonLogin' }">Login with Amazon</router-link>
-            <!-- <router-link :to="{ name: 'AdminLogin' }">Login as Administrator</router-link> -->
             <a href="http://localhost:8000/admin">Login as Administrator</a>
         </div>
         <router-view/>
